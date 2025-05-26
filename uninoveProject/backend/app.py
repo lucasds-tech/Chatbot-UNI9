839a004a436f7c525a83018e30771e914afb5048
lucasds-tech <lucas.dasilva09@hotmail.com>
2025-05-26 18:33:22 -0300

    raw(app.py): include PORT for deploy (1)

--- uninoveProject/backend/app.py
+++ uninoveProject/backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 import sqlite3
 import datetime
 
@@ -112,7 +113,6 @@ def chat():
 
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 #   app.run(debug=True)
